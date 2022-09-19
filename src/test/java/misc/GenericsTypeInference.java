61379452064047e2cd2aa984c789aecc7840bf70
ConsciousPen <109042349+ConsciousPen@users.noreply.github.com>
2022-09-18 23:15:20 -0700

    wildcards

--- src/test/java/misc/GenericsTypeInference.java
+++ src/test/java/misc/GenericsTypeInference.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class GenericsTypeInference {
   /*
-        Compilers ability to look at each methpd invocation and corresponding declaration
+        Compilers ability to look at each method invocation and corresponding declaration
    to determine the type argument/s such as T that make the invocation applicable
 
          the inference algo determines the types of args the type that the result
@@ -20,9 +20,9 @@ public class GenericsTypeInference {
    because of the type inference algorithm we can use instantiation like this :
    List<String> list  = new ArrayList();
 
-   Generic methods and type interference:
-       Enables us to invoke a generic method as you would an ordinary method,
-            without specifying a type between angle brackets
+        Generic methods and type interference:
+    Enables us to invoke a generic method as you would an ordinary method,
+    without specifying a type between angle brackets
    */
 
     public static <T> void addStore(T t, List<Bucket<T>> list) {
---
+++ src/test/java/misc/Wildcards.java
@@ -0,0 +1,14 @@
+package misc;
+
+public class Wildcards {
+        /*
+            Integer is a subtype of Number
+        List<Integer> is NOT a subtype of List<Number>
+
+            Despite the fact that integer is a number
+        because of inheritance
+
+            A LIST<INTEGER> IS NOT A SUBTYPE OF THE LIST<NUMBER>
+        this is a reason we need wildcards. The super type of all kind of types collections are wildcards
+        */
+}
